eac49541f474f323885ce354a4e21a34041fa2bf
James <support-v-jamehe@hotmail.com>
2015-12-22 20:47:52 +0800

    import the ViewModelLocator class and use SimpleIoc to register the pages

---
+++ HappyWorkEveryday/HappyWorkEveryday/ViewModelLocator.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HappyWorkEveryday.ViewModel;
+using HappyWorkEveryday.Pages;
+
+namespace HappyWorkEveryday
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<AskforLeaveViewModel>();
+        }
+
+        public static AskforLeaveViewModel _AskForLeavePage;
+        public static AskforLeaveViewModel AskForLeavePage
+        {
+            get
+            {
+                if (_AskForLeavePage == null)
+                    _AskForLeavePage = ServiceLocator.Current.GetInstance<AskforLeaveViewModel>();
+                return _AskForLeavePage;
+            }
+        }
+    }
+}
