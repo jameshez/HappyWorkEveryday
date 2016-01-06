3eb1e853b36365c275d2e15b879b10feb8dba3c0
Fred Bao <support-v-minbao@hotmail.com>
2016-01-06 11:05:46 +0800

    update the services

---
+++ HappyWorkEveryday/HappyWorkService/CustomModels/OrganizationPageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyWorkService.CustomModels
+{
+    public class OrganizationPageModel
+    {
+        public string Alias { get; set; }
+        public string EnglishName { get; set; }
+        public string TeamName { get; set; }
+        public string TeamLeader { get; set; }
+    }
+}
---
+++ HappyWorkEveryday/HappyWorkService/Services/OrganizationService/OrganizationService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using HappyWorkService.CustomModels;
+using HappyWorkService.Repository;
+
+namespace HappyWorkService.Services.OrganizationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "OrganizationService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select OrganizationService.svc or OrganizationService.svc.cs at the Solution Explorer and start debugging.
+    public class OrganizationService : IOrganizationService
+    {
+        protected static object obj = new object();
+        public IList<OrganizationPageModel> FindOrganizationRelationship()
+        {
+            lock (obj)
+            {
+                try
+                {
+                    var result = (from msdn in DBRepository<Tb_MSDNUser>.db.Tb_MSDNUser.ToList()
+                                  join team in DBRepository<Tb_TeamInfo>.db.Tb_TeamInfo.ToList() on msdn.TeamId equals team.Id
+                                  select new OrganizationPageModel()
+                                  {
+                                      Alias = msdn.Alias,
+                                      EnglishName = msdn.Name,
+                                      TeamName = team.TeamName,
+                                      TeamLeader = team.TeamName
+                                  }).ToList();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+    }
+}
